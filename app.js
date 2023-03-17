9c615e55148444bbfb244b5412f40b1b25691afa
minjeong <kmjeongk@gmail.com>
2023-03-17 15:44:36 +0900

    2023/03/17 로그인구현

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const session = require('express-session');
 
 const app = express();
 const PORT = 4000;
@@ -13,26 +14,40 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(
+  session({
+    secret: '1234',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 1000 * 60 * 60,
+    },
+  }),
+);
 
 const mainRouter = require('./routes');
 const userRouter = require('./routes/users');
 const boardRouter = require('./routes/board');
 const dbRouter = require('./routes/db');
 const dbBoardRouter = require('./routes/dbBoard');
-const cookieRounter = require('./routes/cookie');
+const cookieRouter = require('./routes/cookie');
+const registerRoter = require('./routes/register');
+const loginRouter = require('./routes/login');
 
 app.use('/', mainRouter);
 app.use('/users', userRouter);
 app.use('/board', boardRouter);
 app.use('/db', dbRouter);
 app.use('/dbBoard', dbBoardRouter);
-app.use('/cookie', cookieRounter);
+app.use('/cookie', cookieRouter);
+app.use('/register', registerRoter);
+app.use('/login', loginRouter);
 
 // 에러 던지기
 // 에러가 맨앞에 있으려면 매개변수가 4개 여야함
 app.use((err, req, res, next) => {
   console.log(err.stack);
-  res.status(err.ststusCode);
+  res.status(err.statusCode);
   res.send(err.message);
 });
 
--- routes/cookie.js
+++ routes/cookie.js
@@ -3,12 +3,37 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.cookie('alert', true, {
-    expires: new Date(Date.now() + 1000 * 60),
-    httpOnly: true,
-  });
-  console.log(req.cookies);
-  res.render('index');
+  res.render('cookie');
 });
 
+router.get('/cook', (req, res) => {
+  res.cookie('alert', true, {
+    maxAge: 1000 * 5,
+  });
+  res.status(200);
+  res.json('쿠키 굽기 성공!');
+});
+
+// router.get('/cook', (req, res) => {
+//   //쿠키 발행 코드 작성하기!
+//   res.cookie('alert', true, {
+//     expires: new Date(Date.now() + 1000 * 5),
+//     httpOnly: false,
+//   });
+//   res.render('cookie');
+// });
 module.exports = router;
+// const express = require('express');
+
+// const router = express.Router();
+
+// router.get('/', (req, res) => {
+//   res.cookie('alert', true, {
+//     expires: new Date(Date.now() + 1000 * 60),
+//     httpOnly: true,
+//   });
+//   console.log(req.cookies);
+//   res.render('index');
+// });
+
+// module.exports = router;
